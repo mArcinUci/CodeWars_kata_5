8b6cbffda12a29de4276ab32b75908bac88bb709
mArcinUci <marcinphysio@gmail.com>
2023-11-17 14:18:53 +0100

    all test passed, easy for a 5 kata

--- move_zeros.py
+++ move_zeros.py
@@ -8,4 +8,22 @@ def move_zeros(num):
         new_num.append(0)
     return new_num
 
-print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
+# other solutions from CodeWars
+
+'''
+def move_zeros(array):
+    return [x for x in array if x] + [0]*array.count(0)
+'''
+
+
+'''
+def move_zeros(array):
+    return sorted(array, key=lambda x: x == 0 and x is not False)
+'''
+
+
+'''
+def move_zeros(a):
+    a.sort(key=lambda v: v == 0)
+    return a
+'''
