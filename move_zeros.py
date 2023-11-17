ad27d06998831a32b2b3f46d42c52fa6eee8cb21
mArcinUci <marcinphysio@gmail.com>
2023-11-17 14:15:38 +0100

    all test passed, easy for a 5 kata

--- move_zeros.py
+++ move_zeros.py
@@ -1,2 +1,11 @@
-def move_zeros(hhhhhh):
-    return hhhhhh
+def move_zeros(num):
+    count = 0
+    for i in num:
+        if i == 0:
+            count += 1
+    new_num = [x for x in num if x !=0]
+    for _ in range(count):
+        new_num.append(0)
+    return new_num
+
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
