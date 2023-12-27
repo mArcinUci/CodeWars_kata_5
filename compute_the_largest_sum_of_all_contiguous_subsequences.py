dc0d115256e83799354d0e60ec9f0b417ff3cbce
mArcinUci <mucinski@poczta.onet.pl>
2023-12-27 14:02:13 +0100

    passed 104, 'Execution Timed Out (12000 ms)', need to work on time complexity

--- compute_the_largest_sum_of_all_contiguous_subsequences.py
+++ compute_the_largest_sum_of_all_contiguous_subsequences.py
@@ -1,7 +1,7 @@
 # https://www.codewars.com/kata/56001790ac99763af400008c
 
 def largest_sum(arr):
-    if sum(arr) <= 0 or len(arr) == 0:
+    if len(arr) == 0:
         return 0
     
     largest_sum_list = []
@@ -9,12 +9,9 @@ def largest_sum(arr):
         largest_sum_for_i = []
         new_arr = arr[i:]
         for j in range(len(new_arr)+1):
-            new_arr_j = new_arr[i:j]
-            print(new_arr_j)
+            new_arr_j = new_arr[:j]
             largest_sum_for_i.append(sum(new_arr_j))
-
         largest_sum_list.append(max(largest_sum_for_i))
-
 
     return max(largest_sum_list)
 
