51d04d4cae1d6c43556024598a3cd2f42c46c211
mArcinUci <mucinski@poczta.onet.pl>
2023-12-31 17:23:17 +0100

    passed 12, failed 1. Like i suspected, problem is when first one is unique

--- find_the_unique_string.py
+++ find_the_unique_string.py
@@ -1,15 +1,16 @@
 # https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3
 
 def find_uniq(arr):
-    new_arr = [''.join(set(filter(str.isalpha, elem.lower()))) for elem in arr]
-    new_arr = [''.join(sorted(x)) for x in new_arr]
+    new_arr = [''.join(sorted(set(filter(str.isalpha, elem.lower())))) for elem in arr]
     example = new_arr[0] 
     for i in new_arr[1:]:
-        if i not in example:
+        if i not in example or len(i) != len(example):
             return arr[new_arr.index(i)]
      
         
-
+  
 print(find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']))
 print(find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]))
 print(find_uniq([  'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter']))
+print(find_uniq([ '    ', '  ', ' ', 'a', ' ', '' ]))
+print(find_uniq([ 'foobar', 'barfo', 'fobara', '   ', 'fobra', 'oooofrab' ]))
