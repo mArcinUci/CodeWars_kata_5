bbe7d3decd8208e5230a99f04b4ae6a490a94f3a
mArcinUci <mucinski@poczta.onet.pl>
2023-12-31 16:18:50 +0100

    passed 4 tests, failed 3

---
+++ find_the_unique_string.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3
+
+def find_uniq(arr):
+    new_arr = [''.join(set(filter(str.isalpha, elem.lower()))) for elem in arr]
+    example = new_arr[0] 
+    for i in new_arr[1:]:
+        if i not in example:
+            return arr[new_arr.index(i)]
+     
+        
+
+print(find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']))
+print(find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]))
+print(find_uniq([  'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter']))
