0e5003d9f5104a8acd5db0e54396b2222da62eab
mArcinUci <mucinski@poczta.onet.pl>
2024-01-06 15:19:13 +0100

    naive approach: assuming that grandma was going straight from town to town, passed 104 tests, failed 4

--- help_your_granny.py
+++ help_your_granny.py
@@ -1,21 +1,26 @@
 # https://www.codewars.com/kata/5536a85b6ed4ee5a78000035
 
-def tour(friends, friend_towns, home_to_town_distances):
-    return 0
 
-# [ ["X1", 100.0], ["X2", 200.0], ["X3", 250.0], ["X4", 300.0] ]
 import math
 
-a = 100
-b = 200
-c = 250
-d = 300
+def tour(friends, friend_towns, home_to_town_distances):
+    how_many_friends = len(friends)
+    total_distance = 0
+    keys = list(home_to_town_distances.keys())
 
+    for i in range(len(keys) - 1):
+        value0 = home_to_town_distances[keys[0]]
+        key1 = keys[i]
+        key2 = keys[i + 1]
+        value1 = home_to_town_distances[key1]
+        value2 = home_to_town_distances[key2]
+        total_distance += math.sqrt(value2**2 - value1**2)
+    total_distance = int(total_distance) + value0 + value2
+    return int(total_distance)
+    
 
+friends1 = ["A1", "A2", "A3", "A4", "A5"]
+fTowns1 = [["A1", "X1"], ["A2", "X2"], ["A3", "X3"], ["A4", "X4"]]
+distTable1 = {"X1": 100.0, "X2": 200.0, "X3": 250.0, "X4": 300.0}
 
-f1 = math.sqrt(b**2 - a**2)
-f2 = math.sqrt(c**2 - b**2)
-f3 = math.sqrt(d**2 - c**2)
-
-f = a + f1 + f2 + f3 + d
-print(f)
+print(tour(friends1, fTowns1, distTable1))
