954415eb242ac3bc28e2f44c0da7afc626c156df
mArcinUci <mucinski@poczta.onet.pl>
2024-01-07 16:32:41 +0100

    Done all tests passed, organizing data and Pythagoras in practical application

--- help_your_granny.py
+++ help_your_granny.py
@@ -4,23 +4,43 @@
 import math
 
 def tour(friends, friend_towns, home_to_town_distances):
-    how_many_friends = len(friends)
     total_distance = 0
-    keys = list(home_to_town_distances.keys())
+    friends_and_distance = [[friend, int(home_to_town_distances[town])] for friend, town in friend_towns if friend in friends and town in home_to_town_distances]
+    first_value = friends_and_distance[0][1]
+    last_value = friends_and_distance[-1][1]
 
-    for i in range(len(keys) - 1):
-        value0 = home_to_town_distances[keys[0]]
-        key1 = keys[i]
-        key2 = keys[i + 1]
-        value1 = home_to_town_distances[key1]
-        value2 = home_to_town_distances[key2]
-        total_distance += math.sqrt(value2**2 - value1**2)
-    total_distance = int(total_distance) + value0 + value2
+    for i in range(len(friends_and_distance) - 1):
+        value1 = friends_and_distance[i][1]
+        value2 = friends_and_distance[i+1][1]
+        total_distance += math.sqrt(abs(value2**2 - value1**2))
+    total_distance = total_distance + first_value + last_value
     return int(total_distance)
+
+
+'''
+def tour(friends, friend_towns, home_to_town_distances):
+    res=0
+    s=0
+    for i in friend_towns:
+        if i[0] in friends:
+            res=res+(home_to_town_distances[i[1]]**2-s**2)**(0.5)
+            s=home_to_town_distances[i[1]]
+    res=res+s
+    return int(res)
+'''
+
+
+'''
+def tour(friends, friend_towns, home_to_town_distances):
+    ft = dict(friend_towns)
+    d = [home_to_town_distances[ft[f]] for f in friends if f in ft] + [0]
+    return int(sum((abs(d[i]**2 - d[i-1]**2))**0.5 for i in range(0, len(d))))
+'''
+
+
+'''
+def tour(friends, towns, distances):
+    visits = [distances[i[1]] for i in towns if i[0] in friends]
     
-
-friends1 = ["A1", "A2", "A3", "A4", "A5"]
-fTowns1 = [["A1", "X1"], ["A2", "X2"], ["A3", "X3"], ["A4", "X4"]]
-distTable1 = {"X1": 100.0, "X2": 200.0, "X3": 250.0, "X4": 300.0}
-
-print(tour(friends1, fTowns1, distTable1))
+    return int(visits[0] + sum((b ** 2 - a ** 2) ** .5 for a, b in zip(visits[:-1], visits[1:])) + visits[-1])
+'''
