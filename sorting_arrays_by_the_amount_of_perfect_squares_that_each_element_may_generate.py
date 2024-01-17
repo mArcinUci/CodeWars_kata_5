49db07879bbdff297c090a739ff54ab8549d8149
mArcinUci <mucinski@poczta.onet.pl>
2024-01-17 14:11:50 +0100

    Done, all test passed, i need to understand more lambda sorting solutions:)

--- sorting_arrays_by_the_amount_of_perfect_squares_that_each_element_may_generate.py
+++ sorting_arrays_by_the_amount_of_perfect_squares_that_each_element_may_generate.py
@@ -10,13 +10,34 @@ def is_perfect_square(number):
 
 def sort_by_perfsq(arr):
     count_perfect_square = []
+    answer = []
     for i in arr:
         str_i = str(i)
         all_permutations = permutations(str_i)
         permutations_as_int = {int(''.join(permutation)) for permutation in all_permutations}
         perfect_square_permutations = [x for x in permutations_as_int if is_perfect_square(x)]
         count_perfect_square.append(len(perfect_square_permutations))
-    print(count_perfect_square)
+
+    sorted_pairs = sorted(zip(arr, count_perfect_square), key=lambda pair: (pair[1], -pair[0]), reverse=True)
+    answer = [pair[0] for pair in sorted_pairs]
+    return answer
 
 
-sort_by_perfsq([715, 112, 136, 169, 144])
+'''
+from itertools import permutations
+
+def sort_by_perfsq(arr):
+    return sorted(arr, key = lambda x: [-sum(int("".join(y)) ** 0.5 % 1 == 0 for y in set(permutations(str(x)))), x])
+'''
+
+
+'''
+from itertools import permutations
+from math import isqrt
+
+def perfect_square_perms(n: int) -> int:
+    return sum(isqrt(i) ** 2 == i for i in map(int, map(''.join, set(permutations(str(n))))))
+
+def sort_by_perfsq(arr: list[int]) -> list[int]:
+    return sorted(arr, key=lambda n: (-perfect_square_perms(n), n))
+'''
